0814d33e912709ddb137d45e2f4a4bdcee8e1bd9
Tushar Tandon <tinu2505@gmail.com>
2025-11-16 18:34:12 +0530

    Fix: Update Navbar component to replace header with logo image and adjust CSS styles for better layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
 
   return (
     <nav className={styles.nav}>
-      <a href="/"><h2>THE NEELA SAFARI</h2>{/*<img src="https://q0hao2iwgg.ucarecd.net/c1e6d524-2091-4f10-b423-b0f1f2122032/neelajawaisafari.png" alt="Logo" />*/}</a>
+      <a href="/"><img src="https://q0hao2iwgg.ucarecd.net/c80f86d4-5081-4d49-98f6-69311953993e/neelasafari.png" alt="logo" /></a>
       <button onClick={() => setOpen(!open)} className={styles.hamburger}>
         &#9776;
       </button>
