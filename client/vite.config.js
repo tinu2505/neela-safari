2294d59119bc31afd4fd12f96dd26d0aa9f2e14f
Tushar Tandon <tinu2505@gmail.com>
2025-11-27 20:21:08 +0530

    Fix: Update API endpoints and environment variables for production deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+    allowedHosts:
+       ['neelasafari.com', 'www.neelasafari.com', '.neelasafari.com']
+    },
+
   plugins: [react()],
 })
--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ origin: [
 "http://localhost:5173"
 ],
 methods: ["GET", "POST", "PUT", "DELETE"],
+allowedHeaders: ['Content-Type', 'Authorization'],
 credentials: true
 }));
 app.use(json());
